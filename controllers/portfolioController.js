86a65423bc653e608a7d7eee477bf3644677c33b
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2025-01-30 20:08:27 +0545

    change

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,29 +1,35 @@
-const nodemailer = require('nodemailer');
-const SendmailTransport = require('nodemailer/lib/sendmail-transport');
-const sendEmail = require('sendmail')
+const sendEmail = require('sendmail')();
 
-//transport
-const sendEmail = nodemailer.sendEmail(
-    SendmailTransport({
-        auth:
-    })
-)
-
-const sendEmailController = (req,res) => {
-    try {
-        return res.status(200).send({
-            success: true,
-            message: "Your Message Send Successfully",
-          });
-    } catch (error){
-        console.log(error)
+// controller
+const sendEmailController = (req, res) => {
+  try {
+    sendEmail({
+      from: 'yubrajtimilsina32@gmail.com',
+      to: 'yubrajtimilsina32@gmail.com',
+      subject: 'Hello ✔',
+      html: 'Hello world?',
+    }, function (error, response) {
+      if (error) {
         return res.status(500).send({
-            success: false,
-            message: "Send Email API Error",
-            error,
+          success: false,
+          message: 'Send Email API Error',
+          error,
         });
       }
-};
 
+      return res.status(200).send({
+        success: true,
+        message: 'Your Message Sent Successfully',
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: 'Send Email API Error',
+      error,
+    });
+  }
+};
 
 module.exports = { sendEmailController };
